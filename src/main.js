6a9261e2f681e531de76b27a933881181f207d6b
Nico Ruiz <lifegamer2050@gmail.com>
2023-02-27 00:53:26 +0530

    update

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const { check, validationResult } = require("express-validator");
 const sqlite3 = require("sqlite3").verbose();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const db = new sqlite3.Database("./src/data/urls.db", (err) => {
   if (err) {
     console.error(err.message);
@@ -40,7 +40,10 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
-app.post("/shorten", (req, res) => {
+app.get("/shorten", (req, res) => {
+  res.redirect("/");
+});
+app.post("/api/n/shorten", (req, res) => {
   const longUrl = req.body.url;
   const code = generateShortCode();
   db.run(
@@ -54,11 +57,35 @@ app.post("/shorten", (req, res) => {
         return res.status(500).json({ error: "Database error" });
       }
       const shortUrl = `${req.protocol}://${req.get("host")}/${code}`;
-      res.json({ shortUrl });
+      res.status(200);
+      res.render("success", {
+        shortUrl: shortUrl,
+      });
     }
   );
 });
-app.get("/:code", (req, res) => {
+app.post("/api/s/shorten", (req, res) => {
+  const url = req.body.url;
+  const slug = req.body.slug;
+  db.run(
+    `INSERT INTO urls (code, url) VALUES (?, ?)`,
+    [slug, url],
+    function (err) {
+      if (err) {
+        if (err.message.includes("UNIQUE constraint failed: urls.code")) {
+          return res.status(400).json({ error: "Code already exists" });
+        }
+        return res.status(500).json({ error: "Database error" });
+      }
+      const shortUrl = `${req.protocol}://${req.get("host")}/c/${slug}`;
+      res.status(200);
+      res.render("success", {
+        shortUrl: shortUrl,
+      });
+    }
+  );
+});
+app.get("/c/:code", (req, res) => {
   const code = req.params.code;
   db.get(`SELECT url FROM urls WHERE code = ?`, [code], (err, row) => {
     if (err || !row) {
@@ -66,6 +93,19 @@ app.get("/:code", (req, res) => {
     }
     res.redirect(row.url);
   });
+});
+app.get("/:code", (req, res) => {
+  const code = req.params.code;
+  if (code == "custom") {
+    res.render("custom");
+  } else {
+    db.get(`SELECT url FROM urls WHERE code = ?`, [code], (err, row) => {
+      if (err || !row) {
+        return res.status(404).send("Not found");
+      }
+      res.redirect(row.url);
+    });
+  }
 });
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
